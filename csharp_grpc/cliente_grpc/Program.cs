236b4874f02f67fee5585ae19fb468eff64f5a32
Eugenio Garcia San Martin <egsmartin@gmail.com>
2020-08-23 23:21:20 +0200

    c# funcionando

---
+++ csharp_grpc/cliente_grpc/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using Helloworld;
+using System;
+
+namespace cliente_grpc
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel=GrpcChannel.ForAddress("http://localhost:50051");
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = await client.SayHelloAsync(new HelloRequest
+            {
+                Name= "Eugenio" 
+            });
+
+            Console.WriteLine("From server: "+response.Message);
+        }
+    }
+}
--- env.ps1
+++ env.ps1
@@ -1,3 +1,5 @@
 $ubicacion=pwd
 $env:GOPATH=$env:GOPATH+";"+$ubicacion
+# Con esto cuando hagamos go install xxxxxxxxx dejara el resultado en el directorio bin dentro del pwd
+$env:GOBIB = $ubicacion
 
