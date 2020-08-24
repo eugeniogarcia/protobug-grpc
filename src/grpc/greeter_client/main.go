004e1765585e4dbeddc1dbcd897e3b6b8ebadddc
Eugenio Garcia San Martin <egsmartin@gmail.com>
2020-08-24 06:57:58 +0200

    .

--- src/grpc/greeter_client/main.go
+++ src/grpc/greeter_client/main.go
@@ -29,11 +29,15 @@ func main() {
 	if len(os.Args) > 1 {
 		name = os.Args[1]
 	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
 	defer cancel()
+
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
+
 	log.Printf("Greeting: %s", r.GetMessage())
 }
